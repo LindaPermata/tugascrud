a1120c36bf93bfcf1b8e2268293bf40054832e59
LindaPermata <permatalinda531@gmail.com>
2026-03-15 23:01:18 +0700

    membuat file ValidationUtil

---
+++ src/main/java/com/example/tugascrud/util/ValidationUtil.java
@@ -0,0 +1,25 @@
+package com.example.tugascrud.util;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Validator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+@RequiredArgsConstructor
+public class ValidationUtil {
+
+    private final Validator validator;
+
+    public void validate(Object request) {
+        Set<ConstraintViolation<Object>> constraintViolations = validator.validate(request);
+
+
+        if (!constraintViolations.isEmpty()) {
+            throw new ConstraintViolationException(constraintViolations);
+        }
+    }
+}
